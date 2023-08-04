62d2b60b4e174e8311f550f9f2d2f1287969c13a
Rayanne Ramos <rayanneramosoficial@gmail.com>
2023-08-04 15:02:42 -0300

    mandando notificação para o email

--- App.tsx
+++ App.tsx
@@ -15,6 +15,7 @@ import { Loading } from "./src/components/Loading";
 import { CartContextProvider } from "./src/contexts/CartContext";
 
 OneSignal.setAppId("f8d15092-f1cb-4e45-a3cc-ccc3e631e1d2");
+OneSignal.setEmail("rayanneramos@gmail.com");
 
 export default function App() {
   const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
