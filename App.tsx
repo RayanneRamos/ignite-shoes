aa351570bd4cc9b23d403f84110c5b2da864e540
Rayanne Ramos <rayanneramosoficial@gmail.com>
2023-08-04 16:01:46 -0300

    refatorando o componente de Notificação

--- App.tsx
+++ App.tsx
@@ -5,10 +5,7 @@ import {
   Roboto_400Regular,
   Roboto_700Bold,
 } from "@expo-google-fonts/roboto";
-import OneSignal, {
-  NotificationReceivedEvent,
-  OSNotification,
-} from "react-native-onesignal";
+import OneSignal from "react-native-onesignal";
 
 import { Routes } from "./src/routes";
 
@@ -20,8 +17,6 @@ import {
   tagUserEmailCreate,
   tagUserInfoCreate,
 } from "./src/notifications/notificationsTags";
-import { useEffect, useState } from "react";
-import { Notification } from "./src/components/Notification";
 
 OneSignal.setAppId("f8d15092-f1cb-4e45-a3cc-ccc3e631e1d2");
 OneSignal.setEmail("rayanneramos@gmail.com");
@@ -32,20 +27,9 @@ OneSignal.promptForPushNotificationsWithUserResponse((response) => {
 
 export default function App() {
   const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
-  const [notification, setNotification] = useState<OSNotification>();
 
   tagUserEmailCreate("rayanneramos@gmail.com");
   tagUserInfoCreate();
-
-  useEffect(() => {
-    const unsubscribe = OneSignal.setNotificationWillShowInForegroundHandler(
-      (notificationReceivedEvent: NotificationReceivedEvent) => {
-        const response = notificationReceivedEvent.getNotification();
-        setNotification(response);
-      }
-    );
-    return () => unsubscribe;
-  }, []);
 
   return (
     <NativeBaseProvider theme={THEME}>
@@ -57,12 +41,6 @@ export default function App() {
       <CartContextProvider>
         {fontsLoaded ? <Routes /> : <Loading />}
       </CartContextProvider>
-      {notification?.title && (
-        <Notification
-          title={notification.title}
-          onClose={() => setNotification(undefined)}
-        />
-      )}
     </NativeBaseProvider>
   );
 }
